c9794b8c893a672e2191182f7c1b6837ea2ab8f0
Sergioalf03 <sergioalf03@gmail.com>
2024-01-06 13:14:27 -0700

    Fix: Https

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,6 @@ import { defineCustomElements as jeepSqlite } from 'jeep-sqlite/loader';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
-import 'hammerjs';
 
 if (environment.production) {
   enableProdMode();
